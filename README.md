0f8660c01dd6e0c2215ae0d13324657dd8488c02
Nick Frichette <frichetten@gmail.com>
2018-04-04 16:38:33 -0500

    First prototype Uploaded the first version. It can crawl the JS bundles for static routes. Need to take the time to identify more paterns to search against

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SPA-Directory-Enumerator
 Tool to perform directory enumeration of single page web applications
+
+*For research purposes only! I take no responsibility for misuse*
+
+This tool is in beta, it is not intended to be 100% believed.
+
+At this stage it will generate a lot of false positives, but it can scan the javascript bundles and look for static routes. It cannot detect dynamic routes (this may be framework specific)
+
+## Usage
+./enumerator.py https://(any SPA Website)
+
+Be sure to include http or https.
---
+++ enumerator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# Written by Nick Frichette
+# I take no responsibility for what you do with this tool
+# Intended for security research only
+import requests
+import re # Regular Expressions
+import sys
+
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print("Incorrect usage! Try: ./enumerator.py https://website.com")
+    exit()
+
+link = sys.argv[1]
+r = requests.get(link)
+
+# This will grab all the bundles for inspection
+a = re.findall(r'src=("|\')(.*?)js("|\')', r.text)
+
+for i, item in enumerate(a):
+    if item[1].find('http') != 0:
+        a[i] = link + '/' + item[1] + 'js'
+    else:
+        a[i] = item[1] + 'js'
+
+print("Enumerating Bundles")
+print("-------------------")
+for item in a:
+    print(item)
+
+# For each of these bundles lets scan for directories
+directories = []
+#directories.append("/")
+for item in a:
+    r = requests.get(item)
+    c = re.findall(r'("|\')/(.*?)("|\')', r.text)
+    for direc in c:
+        if len(direc[1]) > 0:
+            if direc[1][0] == '/':
+                directories.append(direc[1])
+            else:
+                directories.append("/"+direc[1])
+
+directories = list(set(directories))
+
+print("")
+print("Enumerating Routes")
+print("------------------")
+for item in directories:
+    print(item)
